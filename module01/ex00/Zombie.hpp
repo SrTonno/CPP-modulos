4f7de92ddd287f36e5d458afeca7e7ca15d6047c
SrTonno <tvillare@student.42madrid.com>
2023-07-12 20:04:36 +0200

    ex01

--- module01/ex00/Zombie.hpp
+++ module01/ex00/Zombie.hpp
@@ -1,37 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/09 18:31:56 by tvillare          #+#    #+#             */
-/*   Updated: 2023/07/09 19:00:49 by tvillare         ###   ########.fr       */
+/*   Created: 2023/07/12 16:06:55 by tvillare          #+#    #+#             */
+/*   Updated: 2023/07/12 16:33:01 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
 #include <iostream>
 #include <string>
 
 class Zombie {
 	private:
 		std::string name;
-
 	public:
 		Zombie(std::string _name);
 		void announce();
 };
+void	randomChump( std::string name );
+Zombie*	newZombie(std::string name);
 
-Zombie::Zombie(std::string _name) {
-	name = _name;
-}
 
-void Zombie::announce() {
-	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
-int main() {
-	Zombie z1("PACO");
-	z1.announce();
-	return 0;
-}
+#endif
--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 18:14:19 by tvillare          #+#    #+#             */
-/*   Updated: 2023/07/09 18:14:20 by tvillare         ###   ########.fr       */
+/*   Updated: 2023/07/12 16:44:14 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+int main(void) {
+	Zombie z("HOAL");
+	Zombie z1("PACO");
+	Zombie z2("Celia");
+	Zombie	*z3;
+	z2.announce();
+	randomChump("Paula");
+	z3 = newZombie("Paula2");
+	z3->announce();
+	delete z3;
+	return 0;
+}
