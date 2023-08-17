169f7ad3e4c50cc0b8a5bbede6e2a737821c04c4
SrTonno <tonovillarejo@gmail.com>
2023-08-17 21:46:06 +0200

    module00

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int j;
+
+    j = 1;
+    while (argc > j){
+        i = 0;
+        while (argv[j][i] != '\0')
+        {
+            argv[j][i] = std::toupper(argv[j][i]);
+            i++;
+        }
+        std::cout << argv[j] << " ";
+        j++;
+    }
+    std::cout << std::endl;
+    return (0);
+}
