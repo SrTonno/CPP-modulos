efec8892577b4bb1a7dba47286bec2baabdb24ec
SrTonno <tvillare@student.42madrid.com>
2023-10-16 19:27:56 +0200

    add ex00 module03

---
+++ module03/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 19:08:25 by tvillare          #+#    #+#             */
+/*   Updated: 2023/10/16 19:27:01 by tvillare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap	a("PACO");
+	ClapTrap	b("MARIA");
+
+	a.attack("MARIA");
+	b.takeDamage(0);
+	b.beRepaired(2);
+	b.takeDamage(20);
+	b.beRepaired(2);
+	return (0);
+}
