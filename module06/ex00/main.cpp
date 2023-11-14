79a2af1aceb3c2cd5002cdad508557c99a835957
SrTonno <tvillare@student.42madrid.com>
2023-11-14 15:43:32 +0100

    ex00 module06

--- module06/ex00/main.cpp
+++ module06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:32:10 by tvillare          #+#    #+#             */
-/*   Updated: 2023/11/14 13:29:08 by tvillare         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:40:19 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,5 @@ int main(int argc, char **argv)
 		return (0);
 	str = argv[1];
 	type = parser(str);
-	std::cout << type << std::endl;
 	ft_print(str, type);
 }
--- module06/ex00/print.cpp
+++ module06/ex00/print.cpp
@@ -6,33 +6,38 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 17:36:58 by tvillare          #+#    #+#             */
-/*   Updated: 2023/11/14 13:45:32 by tvillare         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:41:32 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scaale.hpp"
 #include <cstdlib>
 
-void	print_output(int i, float f, double d)
+void	print_output(float f, double d)
 {
 	std::cout << std::fixed << std::setprecision(2)
-			<< "int:\t "<< i <<"\n"
 			<< "float:\t " << f << "f \n"
 			<< "double:\t " << d
 			<< std::endl;
 }
 
-void	print_overflow(void)
+
+void	write_int(double d)
 {
-	std::cout << std::fixed << std::setprecision(2)
-			<< "int:\t overflow\n"
-			<< "float:\t overflow \n"
-			<< "double:\t overflow"
-			<< std::endl;
+	int	i;
+
+	if (d > 2147483647.0)
+		std::cout << "int:\t Overflow" << std::endl;
+	else if (d < -2147483648.0)
+		std::cout << "int:\t Underflow" << std::endl;
+	else
+	{
+		i = static_cast<int>(d);
+		std::cout << "int:\t " << i << std::endl;
+	}
 }
 
-
-void	print_char(char str) //esta mal
+void	write_char(char str) //esta mal
 {
 	if (str < 33 || str > 127)
 		std::cout << "char: Non displayable" << std::endl;
@@ -68,10 +73,11 @@ void	this_char(std::string str)
 	d = static_cast<double>(i);
 	f = static_cast<float>(i);
 
-	print_char(i);
-	print_output(i, f, d);
+	write_char(i);
+	write_int(i);
+	print_output(f, d);
 }
-
+/*
 void	this_int(std::string str)
 {
 	int		i;
@@ -81,6 +87,7 @@ void	this_int(std::string str)
 	i = std::atoi(str.c_str());
 	d = static_cast<double>(i);
 	f = static_cast<float>(i);
+
 
 	print_char(i);
 	print_output(i, f, d);
@@ -97,23 +104,25 @@ void	this_float(std::string str)
 	i = static_cast<int>(f);
 	print_char(i);
 	print_output(i, f, d);
-}
+}*/
 
 void	this_double(std::string str)
 {
-	int		i;
+	//int		i;
 	double	d;
 	float	f;
 
 	d = std::atof(str.c_str());
 	f = static_cast<float>(d);
-	i = static_cast<int>(d);
-	print_char(d);
-	print_output(i, f, d);
+	//i = static_cast<int>(d);
+
+	write_char(d);
+	write_int(d);
+	print_output(f, d);
 }
 
 
-void	ft_print(const std::string &str, int type) //solucionar overflow
+void	ft_print(const std::string &str, int type)
 {
 	if (type == FLOAT_SPECIAL)
 		especial_float(str);
@@ -121,11 +130,7 @@ void	ft_print(const std::string &str, int type) //solucionar overflow
 		especial_double(str);
 	else if (type == CHAR)
 		this_char(str);
-	else if (type == INT)
-		this_int(str);
-	else if (type == FLOAT)
-		this_float(str);
-	else if (type == DOUBLE)
-		this_float(str);
+	else if (type != ERROR)
+		this_double(str);
 
 }
